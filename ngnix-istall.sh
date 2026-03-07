dafb7c9107bbd2d8a134ca9c373fc819488e0795
Ubuntu <ubuntu@ip-172-31-41-238.eu-north-1.compute.internal>
2026-03-07 21:25:43 +0000

    ngnix installation steps added

---
+++ ngnix-istall.sh
@@ -0,0 +1,14 @@
+#!/bin/bash
+
+## Intro
+
+This will help you to install ngnix and show you a demo page.
+
+## Steps
+
+
+sudo apt-get update
+sudo apt install ngnix
+sudo systemctl start ngnix
+sudo system enable ngnix
+
