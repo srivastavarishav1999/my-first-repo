c7687a3c29e28ea626d2e3ee0574bc3ec1bb196e
Ubuntu <ubuntu@ip-172-31-41-238.eu-north-1.compute.internal>
2026-03-07 21:52:19 +0000

    Chore: Minor changes in script

--- ngnix-istall.sh
+++ ngnix-istall.sh
@@ -1,7 +1,7 @@
 #!/bin/bash
 
 ## Intro
-
+Learning: 
 This will help you to install ngnix and show you a demo page.
 
 ## Steps
@@ -11,4 +11,6 @@ sudo apt-get update
 sudo apt install ngnix
 sudo systemctl start ngnix
 sudo system enable ngnix
+
+echo "NGNIX INSTALLED"
 
